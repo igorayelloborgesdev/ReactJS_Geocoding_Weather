87abfcebc707a004a857920d7caa9c68d1d24498
Igor <igorayelloborgesdev@gmail.com>
2022-04-19 18:52:39 -0300

    Bootstrap installed

--- geo-weather/src/Components/SearchButton.js
+++ geo-weather/src/Components/SearchButton.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React from "react";
+import Button from "react-bootstrap/Button";
 
-const SearchButton = ({ onClick, name }) => (  
-    <button onClick={onClick}>{name}</button>
- )     
+const SearchButton = ({ onClick, name }) => (
+  <div>
+    {/* <button onClick={onClick}>{name}</button> */}
+    <Button variant="primary" onClick={onClick}>{name}</Button>
+  </div>
+);
 export default SearchButton;
--- geo-weather/src/index.js
+++ geo-weather/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 root.render(  
