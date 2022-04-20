6b799837d85a8293dd57e37b9d64a097ba9d4f65
Igor <igorayelloborgesdev@gmail.com>
2022-04-19 23:59:15 -0300

    Maincard

--- geo-weather/src/Components/HighlightCard.js
+++ geo-weather/src/Components/HighlightCard.js
@@ -1,22 +1,37 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card'
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
-const HighlightCard = ({ detailedForecast, namePeriod, shortForecast, temperature, temperatureCelsius, windDirection, windSpeed  }) => (    
-    <div>      
-      <Card border="info">
-      <Card.Body>
-        <Card.Title></Card.Title>
-        <Card.Text>
-          {temperature}°F
-          {detailedForecast}
-    {namePeriod}
-    {shortForecast}
-    {temperatureCelsius}
-    {windDirection}
-    {windSpeed}
-        </Card.Text>
-      </Card.Body>
-    </Card>      
+const HighlightCard = ({
+  detailedForecast,
+  namePeriod,
+  shortForecast,
+  temperature,
+  temperatureCelsius,
+  windDirection,
+  windSpeed,
+}) => (
+  <div>
+    <Card border="info" className={"mainCard"}>
+      <div className={"mainCardChild"}>
+        <div className={"mainCardColLeft"}>
+          <div className={"mainCardColLeftTitle"}>{namePeriod}</div>
+          <div>{temperature}°F</div>
+          <div>{temperatureCelsius}°C</div>
+        </div>
+        <div className={"mainCardColRight"}> 
+          <div>
+            <div className={"mainCardColRightMargin"}>City</div>
+            <div className={"mainCardColRightMargin"}><b>Short forecats:</b> {shortForecast}</div>
+            <div className={"mainCardColRightMargin"}><b>Wind direction:</b> {windDirection}</div>
+            <div className={"mainCardColRightMargin"}><b>Wind speed:</b> {windSpeed}</div>
+            <div className={"mainCardColRightMargin"}><b>Detail:</b> {detailedForecast}</div>
+          </div>                    
+        </div>
+      </div>
+    </Card>
   </div>
 );
 
